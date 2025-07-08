87bd5e2fe6b395b2cffd2a45a9479d2d28445a91
Nikhilesh <nikhileshgowda10@gmail.com>
2025-07-08 14:02:03 -0700

    commit2

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3608;
+const port = 3604;
 
 // Middleware
 app.use(cors());
@@ -153,5 +153,5 @@ app.delete('/api/claims', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running at http://54.237.184.164:${port}`);
+    console.log(`Server running at http://3.85.61.23:${port}`);
 });
